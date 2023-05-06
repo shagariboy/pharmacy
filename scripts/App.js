fbf76f4c8b62376bac23fdae8a4f071b280c1f36
nnamdi ezeh <45914831+shagariboy@users.noreply.github.com>
2023-05-06 21:51:29 +0100

    added testimonial images

--- scripts/App.js
+++ scripts/App.js
@@ -6,3 +6,19 @@ function myFunction() {
       x.className = "navbar";
     }
   }
+
+
+
+
+  function initMap() {
+    // The location of your address
+    var address = {lat: 37.7749, lng: -122.4194}; // Replace with your address
+  
+    // The map, centered at your address
+    var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: address});
+  
+    // The marker, positioned at your address
+    var marker = new google.maps.Marker({position: address, map: map});
+  }
+  
